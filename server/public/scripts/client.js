2807f918123eb36b671bd6510a0592f80110a40e
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-10-13 21:16:35 -0500

    client side function set up in progress

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,10 +4,33 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('jquery loaded');
-    $('.numBtn').on('click', numToInput)
+    $('.numBtn').on('click', numToInput);
+    $('.mathFunction').on('click', mathFunction);
 }
 
+//number inputs being pushed
+var inputOne = [];
 function numToInput() {
-    console.log($(this).val());
-    
+    var numberPushedOne = $(this).val()
+    //console.log(numberPushed);
+    $('.display').append(numberPushedOne);
+    inputOne.push(numberPushedOne);
+    console.log(inputOne);
 }
+
+var mathFun;
+function mathFunction(){
+    var mathFunction = $(this).val();
+    $('.display').empty();
+    mathFun = mathFunction;
+   
+}
+
+numberPushedTwo
+var numberPushedTwo = $(this).val();
+var numberPushedTwo = $(this).val();
+console.log(mathFunction);
+$('.display').empty();
+$('.display').append(numberPushedTwo);
+inputTwo.push(numberPushedTwo);
+console.log(inputTwo);
